28af359415bec08e33fa42399e2acb530e462734
Saul <sjpaguada@gmail.com>
2020-11-07 23:48:20 -0900

    lol

--- src/main/java/Problem3/Problem3.java
+++ src/main/java/Problem3/Problem3.java
@@ -7,8 +7,17 @@ import java.io.PrintStream;
 public class Problem3 {
     public static void printListInReverse(ListNode list, PrintStream print) {
         // homework
+        //got help with this from msquared too
         if(list == null){
+            String empty = "[";
+            print.print(empty.replace("[", ""));
             return;
         }
+        if(list.next == null){
+            print.print(list.val + " ");
+            return;
+        }
+        printListInReverse(list.next, print);
+        print.print(list.val + " ");
     }
 }
